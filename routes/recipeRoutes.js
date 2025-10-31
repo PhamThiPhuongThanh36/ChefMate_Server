28e52ba237e7f1f2bf11f6c641d0e17c1db971e9
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-11-01 01:16:31 +0700

    feat: add route insertCollection, recipe, getcollection

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -4,13 +4,16 @@ const recipeController = require('../controllers/recipeController');
 const { upload, handleMulterError } = require('../middleware/upload');
 
 router.get('/', recipeController.getAll);
-router.get('/search', recipeController.searchByName); // ?name=query
+router.get('/search', recipeController.searchByName);
 router.get('/tags/:tag', recipeController.searchByTag);
 router.get('/user/:userId', recipeController.getByUser);
 router.get('/trending', recipeController.getTrending);
 router.get('/ingredients', recipeController.getAllIngredients);
 router.get('/tags', recipeController.getAllTags);
+router.get('insertCollection', recipeController.insertCollection);
+router.get('/recipe', recipeController.getRecipeById);
 
+router.post('/getcollection', recipeController.getCollection);
 router.post('/create', upload, handleMulterError, recipeController.create);
 
 module.exports = router;
