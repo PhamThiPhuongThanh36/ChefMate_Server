fb4546bddd2e05533d2d70aebb2e7448eae6d5bb
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-11-11 14:53:18 +0700

    feat: update function updateProfile by add image to table Users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const { upload, handleMulterError } = require('../middleware/upload');
 
 router.get('/', userController.getAllUsers);
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.put('/password', userController.changePassword);
-router.put('/:userId/profile', userController.updateProfile);
+router.put('/:userId/profile', upload, handleMulterError, userController.updateProfile);
 
 module.exports = router;
